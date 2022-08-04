4126ef22e48058bcb4058acbefdb8ecbdf0d57de
HenryeBread <80848233+HenryeBread@users.noreply.github.com>
2022-08-03 21:13:12 -0700

    Added the cover art to the Home section. Created a tool bar and some styling changes.

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () =>{
+    return (
+        <nav>
+            <div className="logo">
+            <h1>Flightless</h1>
+            </div>
+            <a>Home</a>
+            <a>Trailer</a>
+            <a>About</a>
+            <a>Characters</a>
+            <a>Flightless Crew</a>
+        </nav>
+    );
+}
+
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Image from 'next/image'
+import Navbar from '@components/Navbar'
 
 export default function Home() {
   return (
     <div className="container">
+      <Navbar />
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Image src="/coverArt.png" width={1000} height={500} />
+        <Header title="Welcome to Arborden!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          This is the beginning of your journey! <code>pages/index.js</code>
         </p>
       </main>
 
