622619fcdfa8becb819719ff42bd27969dc395a1
HenryeBread <80848233+HenryeBread@users.noreply.github.com>
2022-08-09 21:01:16 -0700

    Changed up how the Navbar was created (Used Flexbox). Changed Background Color

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,30 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Image from 'next/image'
-import Navbar from '@components/Navbar'
 
 export default function Home() {
   return (
     <div className="container">
-      <Navbar />
       <Head>
         
+         
+          
       </Head>
 
       <main>
+      <navbar>
+          <ul class="nav-list">
+            <li class="nav-item"><a href="#">Home</a></li>
+            <li class="nav-item"><a href="#">Trailer</a></li>
+            <li class="nav-item"><a href="#">About</a></li>
+            <li class="nav-item"><a href="#">Characters</a></li>
+            <li class="nav-item"><a href="#">Flightless Crew</a></li>
+            </ul>
+        </navbar>
         <Image src="/coverArt.png" width={1000} height={500} />
         <Header title="Welcome to Arborden!" />
         <p className="description">
-          This is the beginning of your journey! <code>pages/index.js</code>
+          This is the beginning of your journey! 
         </p>
       </main>
 
