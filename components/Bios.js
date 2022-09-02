e2dae1633212554173cde5bd83d5dca207e55333
Andrew Maurice <andromulin@gmail.com>
2022-09-02 10:09:11 -0500

    website close to finalized for announcement

--- components/Bios.js
+++ components/Bios.js
@@ -2,10 +2,10 @@ export const Bios = [
     {
         fullName: "Dover Donder",
         homeTown: "Flockaria",
-        aboutMe: "After being separated from his family in a plane crash, the unconfident, flightless bird, Dover, is tasked with reuniting his loved ones and rebuilding thier plane to escape the strange islnad of Arborden. Thankfully,  ",
+        aboutMe: "After being separated from his family in a plane crash, the unconfident, flightless bird, Dover, is tasked with finding his loved ones and rebuilding their plane to escape the quirky island of Arborden. Fortunately for this scaredy-bird, he won't need to handle this daunting task alone! With the island's inhabitants and his mind out to get him, will our feathered-friend sore high or fall short of victory?",
         likes: "Family, Solitude, Comfort",
-        dislikes: "Crowds, Conflict, Clowns",
-        skillAbility: "Team player...?",
+        dislikes: "Crowds, Conflict, His Mind",
+        skillAbility: "Jack of all trades, master of none",
         
         element: "Any",
         colorClass: "null-color",
@@ -18,10 +18,10 @@ export const Bios = [
     {
         fullName: "Golly G. Gooners",
         homeTown: "??????",
-        aboutMe: "Ayo! I'm a bit of a world-traveler. Imma see ALL this world has to offer one day! HMU for memes and beans ;)",
-        likes: "Friends, Memes, Journeys, Beans, Oatmeal, Coffee",
+        aboutMe: "Aye-yo! What up my dudes! The name's Golly! The devs let me write my bio since no one knows me better than... me! Ya boi is the greatest explorer Arborden has ever seen, so I got plenty o' beans ready to spill when ya need some KNOWLEDGE! I'll be vibin' with my feathered-dude, Dover, throughout his whole adventure, so I hope you like memes cause I got loads of 'em ;)",
+        likes: "Friends, Memes, Exploration, Beans, Oatmeal, Coffee",
         dislikes: "Baddies, Restrictions, Fake Fans",
-        skillAbility: "Spilling beans!",
+        skillAbility: "Spills the beans on enemies, locations, and more to teach Dover about Arborden!",
 
         element: "Ocean",
         colorClass: "ocean-color",
@@ -33,10 +33,10 @@ export const Bios = [
     {
         fullName: "Flashy Literson",
         homeTown: "Luminescence",
-        aboutMe: "I'm a flashlight who was raised by lamps. I guess my light is kinda strong? One day, I want to meet other flashlights...",
+        aboutMe: "It's everyone's favorite utility, Flashy the Flashlight! Flashy is the last of the Flashlights in Luminescence. After... the incident, he was taken in as a baby by a nice lamp family, where he was sheltered from the outside world. Despite being a bright guy, he is rather timid and afraid of the outside world. However, when he works up the courage to fight, the Mothia better watch out for his signature LIT move, Flash Fright!",
         likes: "Light, Family, Safety, Bulbalaya",
-        dislikes: "Adventure, Danger, Mothy Stew",
-        skillAbility: "Getting LIT!",
+        dislikes: "Danger, Darkness, Wire Stew",
+        skillAbility: "Gets LIT to brighten rooms, scare certain enemies, and reveal secrets!",
 
         element: "Sky",
         colorClass: "sky-color",
@@ -47,11 +47,11 @@ export const Bios = [
     },
     {
         fullName: "Edrupture",
-        homeTown: "Flockaria",
-        aboutMe: "ABOUT DOVER",
-        likes: "WHAT DOVER LIKES",
-        dislikes: "WHAT DOVER DISLIKES",
-        skillAbility: "Jump High!",
+        homeTown: "Firelight Mines",
+        aboutMe: "You're gonna need earplugs if you wanna talk to Edrupture! This hot-headed fellow can jump higher than your average flightless bird! Ed is never afraid to speak his mind, especially to protect his loved ones! After the zombie rock outbreak in the mines, Ed has been in charge of keeping his fellow volcanites safe, but he'll need more than brute strength and confidence to take out the zomneous threat!",
+        likes: "Productivity, Family, Yelling",
+        dislikes: "Zombies, Slackers, Quiet Places",
+        skillAbility: "Erupt into the ground to jump high up!",
 
         element: "Earth",
         colorClass: "earth-color",
@@ -62,11 +62,11 @@ export const Bios = [
     },
     {
         fullName: "LOTTO (Logical Operations, Terminations, & Troubleshooting Officer)",
-        homeTown: "Flockaria",
-        aboutMe: "ABOUT DOVER",
-        likes: "WHAT DOVER LIKES",
-        dislikes: "WHAT DOVER DISLIKES",
-        skillAbility: "Run fast!",
+        homeTown: "Hacker-Space",
+        aboutMe: "Don't blink, or you'll miss him! Meet, Lotto, everyone's favorite hacker! Whether he uses his pet, Rammy, or his peculiar dagger, Lotto can slice through any bugs that stand in his way! Though he may seem cold and distant at first, Lotto is the most honest RAM you'll ever meet!",
+        likes: "Hacking, Debugging, Coding, Rammy (pet)",
+        dislikes: "Bugs, Loud People, Social Settings",
+        skillAbility: "Use his pet, Rammy, to run faster and jump farther!",
 
         element: "Sky",
         colorClass: "sky-color",
--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ import { faCopyright } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faSteam } from '@fortawesome/free-brands-svg-icons'
+import {faYoutube} from '@fortawesome/free-brands-svg-icons'
 
 
 
@@ -24,12 +25,15 @@ export default function Home() {
           {/* <Image className="homeBackgroundImage" layout='fill' src={"/coverArt.png"} /> */}
         </div>
 
+        <div>
+          <a href="#steam"><h2 className="nav-links-mobile">WISHLIST ON STEAM!</h2></a>
+        </div>
+
         <div id="About">
           <h2>What is Flightless?</h2>
-          <p>Help the flightless bird, Dover, fight his inner demons by teaming up with 7 partners 
-            in this turn-based RPG where your timing matters! Solve puzzles, beat baddies, and tackle platforming 
-            challenges to uncover the mysteries of a quirky, remote island and save Dover's family!</p>
-          <YoutubeEmbed embedId="XeAM_fiWwVY" />
+          <p>Help the flightless bird, Dover, fight his inner demons by teaming up with 7 partners in this turn-based RPG where your timing matters! Solve puzzles, 
+            beat baddies, and tackle platforming challenges to uncover the mysteries of a quirky island, save Dover's family, and fight for better mental health!</p>
+          <YoutubeEmbed embedId="jOq1togfJ9o" />
         </div>
           
         <div id="Gameplay">
@@ -37,17 +41,62 @@ export default function Home() {
           <p>Explore 6 dream-like worlds where you will take on the Mothia, a zombified cult of rock creatures, 
             a corrupt video game company, and more! Uncover the mysteries of the quirky region of Arborden as you 
             aid Dover in his quest to find his family and escape! Don't worry... EVERY WORLD HAS OATMEAL!!!!</p>
+            <div className="gameplay-image-group-holder">
+              {/* <Image className={`gameplay-image`} width="450px" height="253.5px" src="/LD5-spillBeans-opt.gif" />
+              <Image className={`gameplay-image`} width="450px" height="253.5px" src="/LD5-spillBeans-opt.gif" /> */}
+                <div className='gameplay-image-wrap'>
+                  <Image className={`gameplay-image`} width="450px" height="253.5px" src="/SS3-LottoAbility-forest.png" />
+                </div>
+                <div className='gameplay-image-wrap'>
+                  <Image className={`gameplay-image`} width="450px" height="253.5px" src="/SS9-minecart.jpg" />
+                </div>
+            </div>
           <p>
             Engage in turn-based battles featuring Action Commands: a system that requires special inputs, such as 
             timing a button press or hitting buttons in a sequence, to deal more or take less damage in battle. 
             Learn to strategize with the elemental triangle to change how much damage is dealt too by thinking about 
             elemental strengths/weaknesses. 
           </p>
+          <div className="gameplay-image-group-holder">
+              {/* <Image className={`gameplay-image`} width="450px" height="253.5px" src="/LD5-spillBeans-opt.gif" />
+              <Image className={`gameplay-image`} width="450px" height="253.5px" src="/LD5-spillBeans-opt.gif" /> */}
+                <div className='gameplay-image-wrap'>
+                  <Image className={`gameplay-image`} width="450px" height="253.5px" src="/SS2-lavaStompa.png" />
+                </div>
+                <div className='gameplay-image-wrap'>
+                  <Image className={`gameplay-image`} width="450px" height="253.5px" src="/SS8-ThunderWhacked.png" />
+                </div>
+            </div>
           <p>
             You can even equip feathers to give you perks in battles… or make combat <b>MORE DIFFICULT</b> 
             if you crave the extra challenge! Numbers are kept small too to allow you to keep track of how 
             much damage different moves will do more easily in your head than traditional RPGs!
           </p>
+          <div className="gameplay-image-group-holder">
+              {/* <Image className={`gameplay-image`} width="450px" height="253.5px" src="/LD5-spillBeans-opt.gif" />
+              <Image className={`gameplay-image`} width="450px" height="253.5px" src="/LD5-spillBeans-opt.gif" /> */}
+                <div className='gameplay-image-wrap'>
+                  <Image className={`gameplay-image`} width="450px" height="253.5px" src="/SS6-dover-attack-v2.png" />
+                </div>
+                <div className='gameplay-image-wrap'>
+                  <Image className={`gameplay-image`} width="450px" height="253.5px" src="/SS10-Feathers-menu.png" />
+                </div>
+            </div>
+
+            <p>Though Dover's main goals are to save his family and rebuild their airplane, he will also need to work through his internal struggles with mental 
+              health to make that happen. Help Dover gain [b]Confidence Points (CP)[/b] through overcoming battles to unlock his true potential. As 
+              you travel, a mysterious voice will also be tormenting Dover, but it's important to remember that no matter how bad things get, 
+              there is always hope! </p>
+            <div className="gameplay-image-group-holder">
+              {/* <Image className={`gameplay-image`} width="450px" height="253.5px" src="/LD5-spillBeans-opt.gif" />
+              <Image className={`gameplay-image`} width="450px" height="253.5px" src="/LD5-spillBeans-opt.gif" /> */}
+                <div className='gameplay-image-wrap'>
+                  <Image className={`gameplay-image`} width="450px" height="253.5px" src="/SS5-dunder.png" />
+                </div>
+                <div className='gameplay-image-wrap'>
+                  <Image className={`gameplay-image`} width="450px" height="253.5px" src="/SS10-Feathers-menu.png" />
+                </div>
+            </div>
         </div>
 
         <div id="Characters">
@@ -58,23 +107,40 @@ export default function Home() {
         <NewsletterSubscribe />
         
         <div id='PressKit'>
-          <h2>Kit of beans for the press to spill!</h2>
+          <h2>Press Kit full of beans to be spilled!</h2>
           <div>
-            <h3>Assets on Google Drive</h3>
-            <a href="#"><button className='googleDriveLink'>Screenshots</button></a>
-            <a href="#"><button className='googleDriveLink'>Promo Art</button></a>
-            <a href="#"><button className='googleDriveLink'>Trailer</button></a>
+            <h3>Trailer, gifs, screenshots, and more descriptions on Google Drive</h3>
+            <a href="https://drive.google.com/drive/folders/1nvKIo6Y5S8t2zZrMUDxCzysgTEgaoB0q?usp=sharing"><button className='googleDriveLink'>Flightless Press Kit Google Drive</button></a>
           </div>
+          <div>
+            <h3>Quick Facts</h3>
+            <div className='quickFactsPressKit'>
+              <div className='single-fact'><b>Created by:</b> Mole Team 6</div>
+              <div className='single-fact'><b>Published by:</b> Mole Team 6</div> 
+              <div className='single-fact'><b>Promo Art by:</b> <a href='https://pathofpixels.de/'>Path of Pixels</a></div> 
+              <div className='single-fact'><b>Platforms:</b> <a href='#Steam'>Steam</a></div> 
+              <div className='single-fact'><b>Price:</b> TBD</div> 
+              <div className='single-fact'><b>Release:</b> TBD 2024</div> 
+              <div className='single-fact'><b>Languages:</b> English | More TBD</div> 
+              <div className='single-fact'><b>Contact:</b> contact@moleteam6.com</div> 
+              <div className='single-fact'><b>Twitter:</b> <a href='https://twitter.com/flightless_game/'>@flightless_game</a></div> 
+              <div className='single-fact'><b>Youtube:</b> <a href='https://www.youtube.com/channel/UC_bpfFMOLG3nnjErpk_fbPg'>Mole Team 6 Youtube</a></div> 
+            </div>
+          </div>
+          
         </div>
         
       </div>
 
       <div className='copyright-section'>
-        <FontAwesomeIcon icon={faCopyright} />RICE AND RYE GAMES LLC. ALL RIGHTS RESERVED.
+        <FontAwesomeIcon icon={faCopyright} />Mole Team 6 LLC. ALL RIGHTS RESERVED.
       </div>
       <div className='social-section'>
         <a className="single-social-button"  href="https://store.steampowered.com/">
                    <FontAwesomeIcon icon={faSteam} />
+        </a>
+        <a className="single-social-button"  href="https://www.youtube.com/channel/UC_bpfFMOLG3nnjErpk_fbPg">
+                   <FontAwesomeIcon icon={faYoutube} />
         </a>
         <a className="single-social-button"  href="https://twitter.com/flightless_game/">
                    <FontAwesomeIcon icon={faTwitter} />
